b350bd2925484ea043f53e6d37d0c93935eba796
Arafah Ogunfemi <ayobamiarafat09@gmail.com>
2019-12-15 20:04:30 +0100

    updated App to be mobile responsive

--- src/components/header/header.style.jsx
+++ src/components/header/header.style.jsx
@@ -1,10 +1,8 @@
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
-const OptionContainerStyles = css`
-cursor: pointer;
-padding: 10px 15px;  
-`;
+
+
 
 
 export const HeaderContainer = styled.div`
@@ -13,11 +11,22 @@ height: 70px;
     display: flex;
     justify-content: space-between;
     margin-bottom: 25px;
+
+    @media screen and (max-width: 800px) {
+        height: 60px;
+        padding:5px;
+        margin-bottom:20px
+    }
 `
 export const LogoContainer = styled(Link)`
-height: 70%;
+height: 100%;
 width: 70px;
 padding: 25px;
+
+@media screen and (max-width: 800px) {
+    width: 20px;
+    padding:0;
+}
 `
 export const OptionsContainer = styled.div`
 width: 50%;
@@ -25,9 +34,22 @@ height: 100%;
 display: flex;
 align-items: center;
 justify-content: flex-end;
+@media screen and (max-width: 800px) {
+  width:80%;
+  padding-top:1rem
+}
+
 `
 export const OptionLink = styled(Link)`
-${OptionContainerStyles}`
+padding:10px 15px;
+cursor:pointer
+
+@media screen and (max-width: 800px) {
+  
+    padding:10px 5px;
+}`
 
 export const OptionDiv = styled.div`
-${OptionContainerStyles}`
+
+cursor: pointer;
+padding: 10px 15px; `
--- src/pages/homepage/homepage.style.jsx
+++ src/pages/homepage/homepage.style.jsx
@@ -6,4 +6,9 @@ export const HomePageContainer = styled.div`
     flex-direction: column;
     align-items: center;
     padding: 20px 80px;
+
+
+    @media screen and (max-width:800px){
+        padding:10px
+    }
 ` 
