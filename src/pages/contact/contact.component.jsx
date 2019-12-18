5174a23d07cdc8ca86a93bcc39917e26062f3833
Arafah Ogunfemi <ayobamiarafat09@gmail.com>
2019-12-19 00:10:41 +0100

    made signup and signin page responsive

---
+++ src/pages/contact/contact.component.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import './sign-in.style.scss'
+import { connect } from 'react-redux'
+import { googleSignInStart, emailSignInStart } from '../../redux/user/user.actions'
+import FormInput from '../form-input/form-input.component'
+import CustomButton from '../custom-button/custom-button.component';
+
+
+const SignIn = ({emailSignInStart, googleSignInStart}) => {
+    const [userCredentials, setCredentials] = useState({ email: '', password: '' })
+    const { email, password } = userCredentials;
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        
+        emailSignInStart(email, password)
+
+
+    }
+    const handleChange = e => {
+        const { value, name } = e.target;
+        setCredentials({...userCredentials, [name]: value });
+    }
+
+        return (
+            <div className="sign-in">
+                <h2>Get in touch with us !</h2>
+                <span>Sign in with your email and password </span>
+                <form onSubmit={handleSubmit} >
+                    <FormInput
+                        name="email"
+                        type='email'
+                        value={email}
+                        handleChange={handleChange}
+                        label='email'
+                        required 
+                    />
+
+                    <FormInput
+                        name="password"
+                        type='password'
+                        value={password}
+                        handleChange={handleChange}
+                        label='password'
+                        required
+                    />
+                    <div>
+                        <CustomButton type='submit'> Sign in </CustomButton>
+                        <CustomButton type='button' onClick={googleSignInStart} isGoogleSignIn>
+                            Sign in with Google
+            </CustomButton>
+                    </div>
+
+
+                </form>
+
+            </div>
+        )
+    }
+
+
+const mapDispatchToProps = dispatch => ({
+    googleSignInStart: () => dispatch(googleSignInStart()),
+    emailSignInStart: (email, password) => dispatch(emailSignInStart({ email, password }))
+})
+export default connect(null, mapDispatchToProps)(SignIn);
